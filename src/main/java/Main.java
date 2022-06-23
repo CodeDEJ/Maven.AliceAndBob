3f60ae5966211a981f57df5dcf356b59cf246a26
Dejen <dejen@Zipcoders-MacBook-Pro-88.local>
2022-06-23 18:56:37 -0400

    working test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,27 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args ){
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hello. What's your name?");
+        String userName = scanner.nextLine();
+        while (true) {
+            if (userName.equalsIgnoreCase("Alice")) {
+                System.out.println("Hello " + userName + ".");
+                break;
+
+            } else if (userName.equalsIgnoreCase("Bob")) {
+                System.out.println("Hello " + userName + ".");
+                break;
+            } else {
+                System.out.println("");
+            }
+        }
 
     }
 }
